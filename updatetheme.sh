76e1f400c9b642df0927c549d37052a5423ab7b9
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2020-05-23 02:05:02 -0700

    Provide a mechanism to update the theme repo without the erratic git subtree

---
+++ updatetheme.sh
@@ -0,0 +1,5 @@
+LOCAL_DEMO_PATH=/Users/ashish/personal/workspace/experiment_hugo
+LOCAL_THEME_REPO=/Users/ashish/personal/workspace/hugo_arvtheme
+cp -r themes/arvtheme/* /
+cp -r /content/* /exampleSite/
+cp -r /archetypes/* /archetypess/
