64be0cf24aa52eab149839e3faf03486beb737f2
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2020-05-27 09:15:21 -0700

    Add support for customizing bulma

---
+++ mybulma/build_stylesheet.sh
@@ -0,0 +1,2 @@
+sass sass/mystyles.scss:css/arvstyles.css
+cp css/arvstyles.css ../themes/arvtheme/static/css
--- updatetheme.sh
+++ updatetheme.sh
@@ -5,3 +5,4 @@ LOCAL_THEME_REPO=/Users/ashish/personal/workspace/hugo_arvtheme
 cp -r ${LOCAL_DEMO_PATH}/themes/arvtheme/* ${LOCAL_THEME_REPO}/
 cp -r ${LOCAL_DEMO_PATH}/content/* ${LOCAL_THEME_REPO}/exampleSite/
 cp -r ${LOCAL_DEMO_PATH}/archetypes/* ${LOCAL_THEME_REPO}/archetypes/
+cp ${LOCAL_DEMO_PATH}/config.toml ${LOCAL_THEME_REPO}/exampleSite/
